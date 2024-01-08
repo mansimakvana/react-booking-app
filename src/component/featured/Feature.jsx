6c28de802ef0785d02e130cce1cac721f5fb4916
mansimakvana <mansimakvana2@gmail.com>
2024-01-08 15:40:40 +0530

    image problem solve

--- src/component/featured/Feature.jsx
+++ src/component/featured/Feature.jsx
@@ -1,22 +1,25 @@
 import "./feature.css";
+import mumbai from "../../../img/mumbai.jpg";
+import jaipur from "../../../img/jaipur.jpg";
+import newdelhi from "../../../img/newdelhi.jpg";
 
 const Feature = () => {
   return (
     <div className="feature">
       <div className="feature-items">
-        <img src="../../../img/mumbai.jpg" className="feature-img" alt="" />
+        <img src={mumbai} className="feature-img" alt="" />
         <div className="feature-title">
           <h1>Mumbai</h1>
         </div>
       </div>
       <div className="feature-items">
-        <img src="../../../img/jaipur.jpg" className="feature-img" alt="" />
+        <img src={jaipur} className="feature-img" alt="" />
         <div className="feature-title">
           <h1>Jaipur</h1>
         </div>
       </div>
       <div className="feature-items">
-        <img src="../../../img/newdelhi.jpg" className="feature-img" alt="" />
+        <img src={newdelhi} className="feature-img" alt="" />
         <div className="feature-title">
           <h1>New Delhi</h1>
         </div>
--- src/component/homeguest/HomeGuest.jsx
+++ src/component/homeguest/HomeGuest.jsx
@@ -1,10 +1,14 @@
 import "./homeguest.css";
+import homeguest1 from "../../../img/homeguest1.png";
+import homeguest2 from "../../../img/homeguest2.png";
+import homeguest3 from "../../../img/homeguest3.png";
+import homeguest4 from "../../../img/homeguest4.png";
 
 const HomeGuest = () => {
   return (
     <div className="hg">
       <div className="hg-container">
-        <img className="hg-image" src="../../../img/homeguest1.png" alt="" />
+        <img className="hg-image" src={homeguest1} alt="" />
         <span className="hgname">Leman Locke</span>
         <span className="hgcity">London</span>
         <span className="hgprice">Starting from $340</span>
@@ -14,7 +18,7 @@ const HomeGuest = () => {
         </div>
       </div>
       <div className="hg-container">
-        <img className="hg-image" src="../../../img/homeguest2.png" alt="" />
+        <img className="hg-image" src={homeguest2} alt="" />
         <span className="hgname">Vita Apartments</span>
         <span className="hgcity">Italy</span>
         <span className="hgprice">Starting from $128</span>
@@ -24,7 +28,7 @@ const HomeGuest = () => {
         </div>
       </div>
       <div className="hg-container">
-        <img className="hg-image" src="../../../img/homeguest3.png" alt="" />
+        <img className="hg-image" src={homeguest3} alt="" />
         <span className="hgname">Flora Chiado Apartments</span>
         <span className="hgcity">Portugal</span>
         <span className="hgprice">Starting from $405</span>
@@ -34,7 +38,7 @@ const HomeGuest = () => {
         </div>
       </div>
       <div className="hg-container">
-        <img className="hg-image" src="../../../img/homeguest4.png" alt="" />
+        <img className="hg-image" src={homeguest4} alt="" />
         <span className="hgname">Zoku Amsterdam</span>
         <span className="hgcity">Netharlands</span>
         <span className="hgprice">Starting from $120</span>
